2da6dd781fadf44d08bb3ee30d71e1d79b5a2a5a
Mihaja Razafimahefa <50056520+rmihaja@users.noreply.github.com>
2024-04-07 00:04:20 +0200

    add cancer spawn and healthy death

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,7 +1,6 @@
 # main.gd
 extends Node
 
-
 var healthy_cells = []
 var cancer_cells = []
 
@@ -27,18 +26,14 @@ var therapie = ["Chimiotherapie", "Immunotherapie", "Radiotherapie", "TherapieCi
 # Chirurgie -> timed
 # Analyse -> show
 
-
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	spawn_cells(10, "cancer")
 	spawn_cells(100, "healthy")
-	
-	
-	#$CancerCellSpawnTimer.wait_time = cancer_spawning_speed
-	
+	# vitesse d'apparition des cellules cancers
+	$CancerCellSpawnTimer.wait_time = cancer_spawning_speed
+
 	# https://gamedev.stackexchange.com/questions/207387/how-to-get-nested-child-node-without-using-full-path-in-godot
 	#$TargetArea.radius = target_radius
 	# $Camera2D.get_node('TargetArea').radius = target_radius
@@ -48,15 +43,13 @@ func _ready():
 	circle_shape.radius = target_radius
 	# $Camera2D/TargetArea/TargetCollisionShape.shape = circle_shape
 	$TargetArea/TargetCollisionShape.set_shape(circle_shape)
-	
+
 	# print(len(cancer_cells))
 	$TargetArea.connect("area_entered", _on_TargetArea_area_entered)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$TargetArea/TargetCollisionShape.global_position = $Camera2D.get_screen_center_position()
-	#$HealthBar.update_healthy_count(healthy_cells.size())
 	# print($Camera2D.get_screen_center_position())
 	# print($Camera2D/TargetArea/TargetCollisionShape.position)
 	# pass
@@ -80,8 +73,10 @@ func _on_TargetArea_area_entered(area):
 # mort des cellules saines
 func _on_healthy_cell_death_timer_timeout():
 	var index = randi() % healthy_cells.size()
+	remove_child(healthy_cells[index])
 	healthy_cells.remove_at(index)
 	healthy_cell_rids.remove_at(index)
+	$HealthBar.update_healthy_count(healthy_cells.size())
 
 # apparition des cellules cancers
 func _on_cancer_cell_spawn_timer_timeout():
@@ -89,18 +84,14 @@ func _on_cancer_cell_spawn_timer_timeout():
 	cell.global_position = Vector2(randi() % get_viewport().size.x, randi() % get_viewport().size.y)
 	add_child(cell)
 	cancer_cells.append(cell)
-	cancer_cell_rids.append(cell.get_rid())
-
 
 func _on_target_area_target_body_entered(args):
 	#pass # Replace with function body.
 	print('main enter ', args)
 
-
 func _on_target_area_target_body_exited(args):
 	#pass # Replace with function body.
 	print('main exit ', args)
-
 
 func _on_target_area_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	#pass # Replace with function body.
@@ -110,7 +101,6 @@ func _on_target_area_body_shape_entered(body_rid, body, body_shape_index, local_
 	print(healthy_cell_rids.find(body_rid))
 	print(cancer_cell_rids.find(body_rid))
 
-
 func _on_target_area_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
 	#pass # Replace with function body.
 	# print(body.name, ' main exit ; ', body_rid, body)
@@ -118,13 +108,5 @@ func _on_target_area_body_shape_exited(body_rid, body, body_shape_index, local_s
 	if len(target_cell_rids) > 0:
 		target_cell_rids.erase(body_rid)
 
-
-
 func utiliser():
 	pass
-
-
-
-
-func _on_cancel_cell_spawn_timer_timeout():
-	pass # Replace with function body.
