f7dd8348ba240b14aabf118e750e60d6491779ad
Mihaja Razafimahefa <50056520+rmihaja@users.noreply.github.com>
2024-04-06 20:02:54 +0200

    add cancer spawn and healthy death

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,12 +3,15 @@ extends Node
 var healthy_cells = []
 var cancer_cells = []
 var target_radius = 20
+var cancer_spawning_speed = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	spawn_cells(10, "cancer")
 	spawn_cells(100, "healthy")
+	# vitesse d'apparition des cellules cancers
+	$CancerCellSpawnTimer.wait_time = cancer_spawning_speed
 
 	# https://gamedev.stackexchange.com/questions/207387/how-to-get-nested-child-node-without-using-full-path-in-godot
 	$TargetArea.radius = target_radius
@@ -23,15 +26,13 @@ func _ready():
 	# print(len(cancer_cells))
 	$TargetArea.connect("area_entered", _on_TargetArea_area_entered)
 
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$TargetArea/TargetCollisionShape.global_position = $Camera2D.get_screen_center_position()
+	$HealthBar.update_healthy_count(healthy_cells.size())
 	# print($Camera2D.get_screen_center_position())
 	# print($Camera2D/TargetArea/TargetCollisionShape.position)
 	# pass
-	
 
 func spawn_cells(count, type):
 	var scene_path = "res://scenes/cancer_cell.tscn" if type == "cancer" else "res://scenes/healthy_cell.tscn"
@@ -46,3 +47,14 @@ func spawn_cells(count, type):
 
 func _on_TargetArea_area_entered(area):
 	print("Area entered: " + area.name)
+
+# mort des cellules saines
+func _on_healthy_cell_death_timer_timeout():
+	healthy_cells.remove_at(randi() % healthy_cells.size())
+
+# apparition des cellules cancers
+func _on_cancer_cell_spawn_timer_timeout():
+	var cell = load("res://scenes/cancer_cell.tscn").instantiate()
+	cell.global_position = Vector2(randi() % get_viewport().size.x, randi() % get_viewport().size.y)
+	add_child(cell)
+	cancer_cells.append(cell)
