268bc33c0c73def71884e05f0ab7009e67a9bfe9
Anthony <ahmarqej@gmail.com>
2024-04-06 09:05:01 +0200

    Ajout de la vue/scene principale(Main) avec une caméra déplaçable (Camera2D) et des mobs pour tester si la caméra fonctionne

---
+++ scenes/Camera2D.gd
@@ -0,0 +1,25 @@
+extends Camera2D
+
+var speed = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var movement = Vector2()
+
+	if Input.is_action_pressed("camera_right"):
+		movement.x += 1
+	if Input.is_action_pressed("camera_left"):
+		movement.x -= 1
+	if Input.is_action_pressed("camera_down"):
+		movement.y += 1
+	if Input.is_action_pressed("camera_up"):
+		movement.y -= 1
+
+	movement = movement.normalized() * speed * delta
+
+	position += movement
