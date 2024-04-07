d78ac6731dab4a08647a7e75554ab52288096767
Anthony <ahmarqej@gmail.com>
2024-04-07 12:21:11 +0200

    Merge branch 'dev' of https://github.com/phanav/game-jam-2024 into dev

---
+++ scenes/menuGameOver.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_rejouer_bouton_pressed():
+	var main_scene = load("res://scenes/main.tscn").instantiate()
+	get_tree().root.add_child(main_scene)
+	get_tree().current_scene.queue_free()
+	get_tree().set_current_scene(main_scene)
+
+func _on_retour_menu_bouton_pressed():
+	var menu_scene = load("res://scenes/menuDemarrer.tscn").instantiate()
+	get_tree().root.add_child(menu_scene)
+	get_tree().current_scene.queue_free()
+	get_tree().set_current_scene(menu_scene)
